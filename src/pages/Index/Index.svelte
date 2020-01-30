108510734bec45811b52eeda3189fdff77bac286
peterorbb <orbitalist@gmail.com>
2020-01-30 16:32:18 +0300

    Logout controllers

--- src/pages/Index/Index.svelte
+++ src/pages/Index/Index.svelte
@@ -1,12 +1,49 @@
 <script>
+  import axios from 'axios';
   import { Navigate } from 'svelte-router-spa';
 
-  import { deleteTokens } from '../../utilities/tokens';
+  import { deleteTokens, getTokens } from '../../utilities/tokens';
 
   /**
-   * Handle logging out TODO: add API request
+   * Handle logging out from a single device
+   * @returns {*}
    */
-  const logout = () => deleteTokens();
+  const logout = async () => {
+    const { accessToken = '', refreshToken = '' } = getTokens();
+    try {
+      await axios({
+        data: { refreshToken },
+        headers: {
+          'X-ACCESS-TOKEN': accessToken,
+        },
+        method: 'POST',
+        url: 'https://express-mongo-node.herokuapp.com/api/v1/logout',
+      });
+      return deleteTokens();
+    } catch (error) {
+      return deleteTokens();
+    }
+  };
+
+    /**
+   * Handle logging out from all devices
+   * @returns {*}
+   */
+  const logoutAll = async () => {
+    const { accessToken = '' } = getTokens();
+    try {
+      await axios({
+        headers: {
+          'X-ACCESS-TOKEN': accessToken,
+        },
+        method: 'GET',
+        url: 'https://express-mongo-node.herokuapp.com/api/v1/logout/all',
+      });
+      return deleteTokens();
+    } catch (error) {
+      return deleteTokens();
+    }
+  };
 </script>
 
 <div class="page-wrap">
@@ -14,7 +51,8 @@
   <p><Navigate to="login">Login</Navigate></p>
   <p><Navigate to="password-recovery">Password recovery</Navigate></p>
   <p><Navigate to="registration">Registration</Navigate></p>
-  <div on:click={logout}>Log out</div>
+  <div on:click={logout}>Log out from a single device</div>
+  <div on:click={logoutAll}>Log out from all devices</div>
 </div>
 
 <style>
