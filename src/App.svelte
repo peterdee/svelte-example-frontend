63a23d6aa8aac178499ade688c20d2d49988eaf7
peterorbb <orbitalist@gmail.com>
2020-02-04 21:18:20 +0300

    Header component

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
   import { Router } from 'svelte-router-spa';
 
   import Footer from './components/Footer.svelte';
+  import Header from './components/Header.svelte';
   import { routes } from './router';
 </script>
 
+<Header />
 <main>
   <Router { routes } />
 </main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { store } from '../store';
+
+  $: loggedIn = $store.auth.loggedIn;
+</script>
+
+<header class="header-wrap">
+  <div>
+    BlogPost
+  </div>
+  {#if loggedIn}
+    <nav>Logged in</nav>
+  {/if}
+</header>
+
+<style>
+  .header-wrap {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    width: 100%;
+  }
+</style>
