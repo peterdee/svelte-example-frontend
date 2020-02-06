34067384a1dcd257f42fdffb6913438c2a4f184e
peterorbb <orbitalist@gmail.com>
2020-02-06 21:39:00 +0300

    Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,6 +2,15 @@
   import { store } from '../store';
 
   $: loggedIn = $store.auth.loggedIn;
+
+  let showOptions = false;
+
+  const toggleOptions = () => showOptions = !showOptions;
+
+  window.onclick = (event) => {
+    const { target: { id = '' } = {} } = event;
+    showOptions = ((!id || id !== 'options') && !showOptions) || showOptions;
+  };
 </script>
 
 <header class="header-wrap noselect">
@@ -12,15 +21,47 @@
         BlogPost
       </div>
     </div>
-    <div>
+    <div class="header-left">
       {#if loggedIn}
-        <nav>Logged in</nav>
+        <div>
+          <div
+            class="cog-logo"
+            on:click={toggleOptions}
+          ></div>
+          {#if showOptions}
+            <div
+              class="options"
+              id="options"
+            >
+              <div>
+                <a href="/account">Account</a>
+              </div>
+              <div>
+                <a href="/logout">Logout</a>
+              </div>
+            </div>
+          {/if}
+        </div>
+      {:else}
+        <div>
+          <a href="/login">Login</a>  
+        </div>
+        <div class="margin-left">
+          <a href="/registration">Registration</a>  
+        </div>
       {/if}
     </div>
   </div>
 </header>
 
 <style>
+  .cog-logo {
+    background: url(../assets/gear.png) no-repeat center;
+    border-radius: 50%;
+    cursor: pointer;
+    height: 50px;
+    width: 50px;
+  }
   .header-boundaries {
     align-items: center;
     display: flex;
@@ -28,6 +69,7 @@
     height: 80px;
     justify-content: space-between;
     margin: 0 35px;
+    max-width: 768px;
     width: 100%;
   }
   .header-branding {
@@ -42,7 +84,7 @@
     justify-content: flex-start;
   }
   .header-logo {
-    background: url(../assets/logo.png) no-repeat center;
+    background: url(../assets/logo.gif) no-repeat center;
     border-radius: 50%;
     height: 50px;
     width: 50px;
@@ -52,5 +94,17 @@
     flex-direction: row;
     justify-content: center;
     width: 100%;
+  }
+  .margin-left {
+    margin-left: 15px;
+  }
+  .options {
+    background-color: #f0f0f0;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    z-index: 1;
   }
 </style>
