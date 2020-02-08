efd6b75a417fd6921918153c2ab96c1eb3c50a99
peterorbb <orbitalist@gmail.com>
2020-02-08 22:40:39 +0300

    Profile page

---
+++ src/pages/Profile/PasswordForm.svelte
@@ -0,0 +1,78 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  import Button from '../../reusable/Button.svelte';
+  import Error from '../../reusable/Error.svelte';
+  import Input from '../../reusable/Input.svelte';
+
+  export let confirmNewPasswordHighlight = '';
+  export let formError = '';
+  export let isLoading = false;
+  export let newPasswordHighlight = '';
+  export let oldPasswordHighlight = '';
+
+  const dispatch = createEventDispatcher();
+
+  // handle form submit
+  const handleForm = () => dispatch('handle-form');
+
+  // handle text inputs
+  const handleInput = ({ detail: { target: { name = '', value = '' } = {} } = {} }) => dispatch('handle-input', {
+    name,
+    value,
+  });
+</script>
+
+<form
+  class="password-form"
+  on:submit|preventDefault={handleForm}
+>
+  <div class="margin">
+    <Input
+      disabled={isLoading}
+      highlight={oldPasswordHighlight}
+      name="oldPassword"
+      placeholder="Old Password"
+      type="password"
+      on:handle-input={handleInput}
+    />
+  </div>
+  <div class="margin">
+    <Input
+      disabled={isLoading}
+      highlight={newPasswordHighlight}
+      name="newPassword"
+      placeholder="New Password"
+      type="password"
+      on:handle-input={handleInput}
+    />
+  </div>
+  <div class="margin">
+    <Input
+      disabled={isLoading}
+      highlight={confirmNewPasswordHighlight}
+      name="confirmNewPassword"
+      placeholder="Confirm New Password"
+      type="password"
+      on:handle-input={handleInput}
+    />
+  </div>
+  <Button
+    disabled={isLoading}
+    text="Update"
+    type="submit"
+  />
+  <Error message={formError} />
+</form>
+
+<style>
+  .password-form {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    max-width: 349px;
+  }
+  .margin {
+    margin-bottom: 15px;
+  }
+</style>
