ab39b18b0031bc3c22a265144f376a9918dd9910
peterorbb <orbitalist@gmail.com>
2020-02-22 20:23:57 +0300

    Profile page: Change Email section

--- src/pages/Profile/ChangeEmailSection.svelte
+++ src/pages/Profile/ChangeEmailSection.svelte
@@ -36,13 +36,64 @@
    */
   const handleForm = async () => {
     // check the data
-    
+    const keys = Object.keys(data);
+    formMessage.type = 'error';
+    if (keys.some((key = '') => !data[key])) {
+      keys.forEach((key = '') => highlight[key] = (!data[key] && 'error') || '');
+      return formMessage.message = 'Please provide the necessary data!';
+    }
+
+    const { accessToken = '' } = getTokens();
+
+    // highlight inputs, start loading
+    formMessage = {
+      message: '',
+      type: '',
+    };
+    keys.forEach((key = '') => highlight[key] = 'success');
+    handleLoader(true);
 
     try {
+      await axios({
+        data,
+        headers: {
+          'X-ACCESS-TOKEN': accessToken,
+        },
+        method: 'POST',
+        url: 'https://express-mongo-node.herokuapp.com/api/v1/change-email/send-link',
+      });
 
+      formMessage = {
+        message: 'Verification link was sent!',
+        type: 'info',
+      };
+      return handleLoader(false);
     } catch (error) {
-      // remove the loader
-      isLoading = false;
+      handleLoader(false);
+      formMessage.type = 'error';
+      const { response: { data: { data = {}, info = '', status = null } = {} } = {} } = error;
+      if (status === 400) {
+        if (info === 'INVALID_DATA' || info === 'MISSING_DATA') {
+          const errorType = info.split('_')[0].toLowerCase();
+          const fields = data[errorType] || [];
+          fields.forEach((field = '') => highlight[field] = 'error');
+          return formMessage.message = `Provided data is ${errorType}!`;
+        }
+      }
+      if (status === 401 && matchingInfo.includes(info)) {
+        return refreshTokens();
+      }
+      if (status === 403 && info === 'EMAIL_ALREADY_IN_USE') {
+        keys.forEach((key = '') => highlight[key] = 'error');
+        return formMessage.message = 'Email address is already in use!';
+      }
+
+      keys.forEach((key = '') => highlight[key] = '');
+      if (info === 'INTERNAL_SERVER_ERROR' && status === 500) {
+        return formMessage.message = 'Oops! Something went wrong...';
+      }
+
+      return formMessage.message = 'Access denied!';
     }
   };
 
@@ -59,7 +110,7 @@
       type: '',
     };
     return highlight[name] = '';
-  }
+  };
 </script>
 
 <div class="section-title margin noselect">
